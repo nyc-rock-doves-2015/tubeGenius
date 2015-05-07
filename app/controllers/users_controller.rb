28e82baf75fe75de95a1366240df7340e4a7770f
Sabron13 <kurt.schnelker@gmail.com>
2015-05-07 18:18:23 -0400

    adds actions for new, create, show, and user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+
+    if @user.valid?
+      session_in!(@user)
+      redirect_to root_path
+    else
+      set_flash "Bad sign up information"
+      redirect_to new_user_path
+    end
+  end
+
+  def show
+    @user = user.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
 end
