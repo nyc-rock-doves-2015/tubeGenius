54acd77bfa17e12a31f3c9105f4c7c62298a8920
Alexander Mihalopoulos <amihalop@gmail.com>
2015-05-07 18:33:45 -0400

    make comments#create #new #show controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,26 @@
 class CommentsController < ApplicationController
+  def new
+    @video = Video.find(params[:id])
+    @comment = @video.comments.new
+  end
+
+  def create
+    @comment = Comment.new(params[:comment])
+
+    if @comment.save
+      redirect_to video_path(@comment.video)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :start_time, :end_time, :media_url, :media_type).merge(user: current_user)
+  end
 end
