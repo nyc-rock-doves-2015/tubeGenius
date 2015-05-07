e6eac3bdccfc239734e7c3532d0af22fc8f47c29
Sabron13 <kurt.schnelker@gmail.com>
2015-05-07 18:55:29 -0400

    adds user auth methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(name: params[:user][:name])
     if @user && @user.authenticate(params[:user][:password])
       session_in!(@user)
-      redirect_to root_path
+      redirect_to user_path(@user.id)
     else
       set_flash("Bad login information")
       redirect_to new_session_path
