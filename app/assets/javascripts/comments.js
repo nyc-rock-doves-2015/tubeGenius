451311857b792a10be43921d2452cc330794d3ad
costolo <ben.costolo@gmail.com>
2015-05-08 14:45:04 -0400

    Fixed capitalization

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -13,18 +13,18 @@ App.getComments = function (video_id) {
   return json_data;
 };
 
-App.popcorn = function (video_url, video_container, comment_array) {
+App.Popcorn = function (video_url, video_container, comment_array) {
   var vid_setup = Popcorn.HTMLYouTubeVideoElement(video_container);
   vid_setup.src = video_url + "&controls=2"
   this.video = Popcorn(vid_setup);
   this.comments = comment_array;
 }
 
-//App.popcorn.prototype.showComments = function () {}
+//App.Popcorn.prototype.showComments = function () {}
 
 $(function () {
   var video_id = parseInt(window.location.href.match(/\d+$/));
   var results = App.getComments(video_id)
-  var video = new App.popcorn(results[0], "#video", results[1])
+  var video = new App.Popcorn(results[0], "#video", results[1])
   console.log("loaded")
 });
