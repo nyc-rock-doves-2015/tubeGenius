5a529587677cd8d52587342b23f48be6b86cf017
costolo <ben.costolo@gmail.com>
2015-05-08 19:48:54 -0400

    Toggle comment form via button

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -30,9 +30,33 @@ App.Popcorn.prototype.showComments = function () {
   }
 }
 
+App.Popcorn.prototype.addComment = function () {
+  $('.new_comment').on("submit", function (event) {
+    var self = this;
+    event.preventDefault();
+    var $target = $(event.target);
+    console.log($target)
+    $.ajax({
+      url: $target.attr("action"),
+      type: 'post',
+      data: $target.serialize()
+    }).done(function (response) {
+      // self.comments.push({start: parseInt($target[0][3].value), 
+      //   end: parseInt($target[0][4].value), text: $target[0][2].value, 
+      //   target: "com"});
+      // self.showComments();
+      $(".new_comment").toggle();
+    })
+  })
+}
+
 $(function () {
   var video_id = parseInt(window.location.href.match(/\d+$/));
-  var results = App.getComments(video_id)
-  var video = new App.Popcorn(results[0], "#video", results[1])
+  var results = App.getComments(video_id);
+  var video = new App.Popcorn(results[0], "#video", results[1]);
   video.showComments();
+  video.addComment();
+  $('#comment_button').on("click", function () {
+    $(".new_comment").toggle();
+  })
 });
