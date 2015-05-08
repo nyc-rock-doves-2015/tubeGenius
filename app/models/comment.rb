22330e140cfe1ed13c47092839391e0dc11cb482
Alexander Mihalopoulos <amihalop@gmail.com>
2015-05-08 15:41:56 -0400

    add comment validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,4 +6,6 @@ class Comment < ActiveRecord::Base
   belongs_to :parent, class_name: "Comment"
   has_many :children, class_name: "Comment", foreign_key: :parent_id
 
+  validates :content, presence: true
+
 end
