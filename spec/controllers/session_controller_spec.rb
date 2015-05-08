b242a76c49dd1b118b30bb45e309a1c75a47b04f
Sabron13 <kurt.schnelker@gmail.com>
2015-05-07 20:41:18 -0400

    required rails helper, set up structure for tests

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -1,25 +1,32 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe SessionsController do
   
   describe 'GET #new' do
-    it "renders the :new view"
+    it "renders the :new view"do
+    end
   end
 
   describe 'POST #create' do
     context 'with valid attributes' do
-      it "creates a new session with the user's id"
-      it "redirects to the user's :show page"
+      it "creates a new session with the user's id"do
+      end
+      it "redirects to the user's :show page"do
+      end
     end
 
     context 'with invalid attributes' do
-      it "does not create a new session"
-      it "flashes an error message"
+      it "does not create a new session"do
+      end
+      it "flashes an error message"do
+      end
     end
   end
 
   describe 'DELETE #destroy' do
-    it "signs a user out"
-    it "redirects to root_path"
+    it "signs a user out" do
+    end
+    it "redirects to root_path"do
+    end
   end
 end
