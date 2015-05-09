363bd2dd1eb8da10b1e4d5f05cb1b7fd40aed3ff
Alexander Mihalopoulos <amihalop@gmail.com>
2015-05-08 20:28:28 -0400

    add new_comment_path for subcomments

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,10 @@ Rails.application.routes.draw do
     get 'json' => 'videos#json_url_and_comments', as: 'json'
 
   end
-
   resource :session, :only => [:new, :create, :destroy]
   resources :users, :except => [:destroy]
+
+  get '/comments/new/(:parent_id)', to: 'comments#new', as: :new_comment
 
   get 'signin' => 'sessions#new', as: 'signin'
   get 'signout' => 'sessions#destroy', as: 'signout'
