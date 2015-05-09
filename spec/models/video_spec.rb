4d01e80181416187f2bb71fec70d2d1532097208
Carl Swanson <cetswanson@gmail.com>
2015-05-09 19:54:47 -0400

    Add title validation check to video model spec

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -9,4 +9,10 @@ describe Video do
   it "should have valid factory" do
     expect(@video).to be_valid
   end
+
+  it "is invalid without a title" do
+    expect{create(:video, :title => nil)}.to raise_error
+  end
+
+
 end
