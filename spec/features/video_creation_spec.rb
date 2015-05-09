49cea58723ed7c7612d697527bee3ef3db98d205
Carl Swanson <cetswanson@gmail.com>
2015-05-09 11:03:04 -0400

    Replace user signin with rack session setting in video creation feature tests

--- spec/features/video_creation_spec.rb
+++ spec/features/video_creation_spec.rb
@@ -4,10 +4,8 @@ RSpec.feature 'New video', :type => :feature do
   let!(:user) { User.create(name: "username", password: 'password', password_confirmation: 'password')}
 
   scenario "User creates a new video" do
-   visit signin_path
-   fill_in "user[name]", :with => "test_user"
-   fill_in "user[password]", :with => "password"
-   click_on "Sign In"
+   page.set_rack_session(user_id: user.id)
+
     visit "/videos/new"
       fill_in "video_title", :with => "Test Title"
       fill_in "video_url", :with => "https://www.youtube.com/watch?v=ckpwSAv5we8"
@@ -17,10 +15,7 @@ RSpec.feature 'New video', :type => :feature do
   end
 
   scenario "User sets the commentability status of a new video" do
-    visit signin_path
-    fill_in "user[name]", :with => "test_user"
-    fill_in "user[password]", :with => "password"
-    click_on "Sign In"
+    page.set_rack_session(user_id: user.id)
 
     visit "/videos/new"
      fill_in "video_url", :with => "https://www.youtube.com/watch?v=ckpwSAv5we8"
@@ -31,10 +26,7 @@ RSpec.feature 'New video', :type => :feature do
   end
 
   scenario "User creates a video without a URL" do
-    visit signin_path
-    fill_in "user[name]", :with => "test_user"
-    fill_in "user[password]", :with => "password"
-    click_on "Sign In"
+    page.set_rack_session(user_id: user.id)
 
     visit "/videos/new"
      click_button "Create Video"
