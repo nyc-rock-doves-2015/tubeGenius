38371dfdfaae5d80dd9fd087b464e2e42e346cde
Carl Swanson <cetswanson@gmail.com>
2015-05-10 17:34:30 -0400

    Add feature spec for non-owner user deleting video

--- spec/features/video_deletion_spec.rb
+++ spec/features/video_deletion_spec.rb
@@ -4,6 +4,13 @@ RSpec.feature 'New video', :type => :feature do
   let!(:user) { create(:user) }
   let!(:video_permission) { create(:video, :user_id => 10) }
 
+  scenario "User cannot delete a video if they do not own it" do
+    page.set_rack_session(user_id: user.id)
+
+    visit video_path(video_permission)
+    expect(page).not_to have_text("Delete Video")
+  end
+
   scenario "User can delete a video" do
    page.set_rack_session(user_id: user.id)
 
