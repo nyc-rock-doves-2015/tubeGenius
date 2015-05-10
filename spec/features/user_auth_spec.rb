d8c251cc30a02ffbb7682fa8fb19b058dc223a27
Carl Swanson <cetswanson@gmail.com>
2015-05-10 15:04:18 -0400

    Add user sign up page

--- spec/features/user_auth_spec.rb
+++ spec/features/user_auth_spec.rb
@@ -12,4 +12,14 @@ RSpec.feature 'User', :type => :feature do
     visit "/signin"
     expect(page).to have_text("Sign In")
   end
+
+  scenario "User can sign up" do
+    visit "/signup"
+    fill_in "user_name", :with => "test_user"
+    fill_in "user_password", :with => "password"
+    fill_in "user_password_confirmation", :with => "password"
+    click_button "Sign Up"
+
+    expect(page).to have_text("TubeGenius Profile")
+  end
 end
