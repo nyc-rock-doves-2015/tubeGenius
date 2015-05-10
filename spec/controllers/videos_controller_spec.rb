984cfc2c395104f173d3168ca766054162cdba9c
Carl Swanson <cetswanson@gmail.com>
2015-05-10 13:15:35 -0400

    Replace user create with factory in video controller rspec

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe VideosController do
-  let!(:user) { User.create(name: "username", password: 'password', password_confirmation: 'password')}
+  let(:user) { create(:user) }
 
   it "renders the :new view" do
     get :new
