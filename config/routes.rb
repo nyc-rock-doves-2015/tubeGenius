65b2ac5df7e23b444e448ff7f138f1177537f9e8
Alexander Mihalopoulos <amihalop@gmail.com>
2015-05-12 09:46:57 -0400

    add restore and unread routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   resources :conversations, only: [:index, :show, :destroy] do
     member do
       post :reply
+      post :restore
+      post :mark_as_read
     end
   end
   resources :messages, only: [:new, :create]
