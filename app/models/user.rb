2b9d96a4c526157dd24269a2dbdafbdc4527cee2
Carl Swanson <cetswanson@gmail.com>
2015-05-12 15:11:46 -0400

    Delete mailboxer comments in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,13 +13,4 @@ class User < ActiveRecord::Base
   has_many :messages, through: :message_users
 
 
-#   #Returning any kind of identification you want for the model
-# def name
-#   return "hello"
-# end
-# #Returning the email address of the model if an email should be sent for this object (Message or Notification).
-# #If no mail has to be sent, return nil.
-def mailboxer_email(object)
-  email
-end
 end
