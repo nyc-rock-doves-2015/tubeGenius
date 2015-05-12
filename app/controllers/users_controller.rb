716194b29d69fec0e0ad600f3580752ae6a812ed
costolo <ben.costolo@gmail.com>
2015-05-12 15:15:11 -0400

    Add conditional to render profile partial on ajax request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    if request.xhr?
+      render 'users/_profile', layout: false
+    end
   end
 
   private
