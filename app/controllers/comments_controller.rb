404b36603fc02170fde4fe98ab4d0bd80bf77e4a
Carl Swanson <cetswanson@gmail.com>
2015-05-12 14:46:40 -0400

    Refactor find_commentable method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -74,9 +74,7 @@ class CommentsController < ApplicationController
 
   def find_commentable
     params.each do |name, value|
-      if name =~ /(.+)_id$/
-        return $1.classify.constantize.find(value)
-      end
+      return $1.classify.constantize.find(value) if name =~ /(.+)_id$/
     end
     nil
   end
