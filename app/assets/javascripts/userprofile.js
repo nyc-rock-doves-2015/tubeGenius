7f8b8a2c80cdb8cbcd6bcf8481bfb20da2cb1235
costolo <ben.costolo@gmail.com>
2015-05-13 13:28:43 -0400

    Change target element in ajax call so it would work from all pages

--- app/assets/javascripts/userprofile.js
+++ app/assets/javascripts/userprofile.js
@@ -1,11 +1,10 @@
 $(function () {
-  $("#message-button").on("click", function (event) {
+  $(document).on("click", "#message-button", function (event) {
     var $target = $(event.target)
     $.ajax({
       url: $target.attr("href"),
       type: 'get'
     }).done(function (response) {
-      console.log(response)
       $('#sendmessage-modal').html(response);
     })
   })
