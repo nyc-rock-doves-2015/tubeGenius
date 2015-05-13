4552f07b218d4f306d6b8233c1111291c942e2ba
Alexander Mihalopoulos <amihalop@gmail.com>
2015-05-13 14:36:44 -0400

    switch unrea_notifs to not be overwritten after update_attr

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,9 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.notifications
+    @notifications = current_user.notifications.order('created_at DESC')
+    @unread_notifs = []
     @notifications.each do |notification|
+      @unread_notifs << notification if notification.read == false
       notification.update_attribute(:read, true)
     end
   end
