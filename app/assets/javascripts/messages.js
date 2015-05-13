9a77245dacd4240d4f6ef1ccf108472f48c72cb5
costolo <ben.costolo@gmail.com>
2015-05-13 11:35:15 -0400

    Add document ready to messages.js

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,32 +1,35 @@
-$('#inbox-link').on("click", function (event) {
+$(function () {
+  $('#inbox-link').on("click", function (event) {
+      event.preventDefault();
+      $.ajax({
+        url: '/conversations?box=inbox',
+        type: 'get'
+      }).done(function (response) {
+        $('#messages').html(response);
+        $('#box-type').html("Inbox")
+      });
+    });
+
+  $('#sent-link').on("click", function (event) {
     event.preventDefault();
     $.ajax({
-      url: '/conversations?box=inbox',
+      url: '/conversations?box=sent',
       type: 'get'
     }).done(function (response) {
       $('#messages').html(response);
-      $('#box-type').html("Inbox")
+      $('#box-type').html("Sent messages")
     });
   });
 
-$('#sent-link').on("click", function (event) {
-  event.preventDefault();
-  $.ajax({
-    url: '/conversations?box=sent',
-    type: 'get'
-  }).done(function (response) {
-    $('#messages').html(response);
-    $('#box-type').html("Sent messages")
+  $('#trash-link').on("click", function (event) {
+    event.preventDefault();
+    $.ajax({
+      url: '/conversations?box=trash',
+      type: 'get'
+    }).done(function (response) {
+      $('#messages').html(response);
+      $('#box-type').html("Trash")
+    });
   });
-});
-
-$('#trash-link').on("click", function (event) {
-  event.preventDefault();
-  $.ajax({
-    url: '/conversations?box=trash',
-    type: 'get'
-  }).done(function (response) {
-    $('#messages').html(response);
-    $('#box-type').html("Trash")
-  });
-});
+})  
+  
