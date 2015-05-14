90d4c83a4706f666bd820e98cfd45bbceca62fae
costolo <ben.costolo@gmail.com>
2015-05-13 21:05:20 -0400

    update production.rb with action mailer configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,10 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.action_mailer.perform_deliveries = true
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
