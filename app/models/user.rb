d60e35cdb6cb22b0a3a341fca367b4e79f030379
Carl Swanson <cetswanson@gmail.com>
2015-05-19 20:26:17 -0400

    Add email validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,10 @@ class User < ActiveRecord::Base
   gravtastic
   validates :name, presence: true, uniqueness: true, length: { maximum: 50 }
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 
   has_many :videos
   has_many :comments
